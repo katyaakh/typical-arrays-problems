ac05f0d789b0296ca387b92bc1315755cf80026b
Katya Akh <ekatakhm@gmail.com>
2020-09-30 17:06:48 +0300

    init: submit solution TAP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  var min = array[0];
+  
+  for(var i=1; i < array.length; i++) {
+    if(array[i] < min){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  var max = array[0];
+  for(i=1; i<array.length;i++){
+    if(array[i] > max){
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  var avg = array[0];
+  for(i=1; i<array.length; i++) {
+    avg = avg + array[i];
+  }
+  return (avg / array.length);
 }
